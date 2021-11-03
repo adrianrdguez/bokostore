0503234eb68ada45f4b9835d1e64cd687d407d4d
adrianrdguez <adrian.rodriguez@singularfactory.com>
2021-11-03 09:45:10 +0100

    basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,32 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        md: ['16px'],
+        md1: ['18px'],
+        md2: ['20px'],
+        lg: ['22px'],
+        lg1: ['23px'],
+        lg2: ['29px'],
+        xl: ['30px'],
+        xl1: ['32px'],
+        xl2: ['34px'],
+      },
+      textColor: {
+        'secondary': '#9B9B9B',
+        'primary': '#656565',
+        'blackest': '#000000',
+        'category': '#1D1D1D'
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+      textColor: ['active']
+    }
   },
   plugins: [],
 }
